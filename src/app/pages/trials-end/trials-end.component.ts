c8ed7abb55566c3715bb5b370a4afde7f0be23c4
Tati Youcef <youcef.tati04@gmail.com>
2025-02-07 21:17:54 +0100

    Gift clock

--- src/app/pages/trials-end/trials-end.component.ts
+++ src/app/pages/trials-end/trials-end.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-trials-end',
@@ -7,8 +7,36 @@ import { Component } from '@angular/core';
   templateUrl: './trials-end.component.html',
   styleUrl: './trials-end.component.css'
 })
-export class TrialsEndComponent {
+export class TrialsEndComponent implements OnInit {
 
   isOpened = false;
+
+  hDeg = 14 * 30 + 14 * (360/720);
+  mDeg = 14 * 6 + 14 * (360/3600);
+  sDeg = 14 * 6;
+
+  emptyList = Array<number>(59);
+
+  openLock = new Audio('../../../assets/Sounds/OpenLock.mp4');
+  keyObtainedSound = new Audio('../../../assets/Sounds/KeyGotten.mp4');
+
+  ngOnInit(): void {
+
+    for (let i = 1; i < 60; i++) {
+      this.emptyList[i] = i;
+    }
+
+  }
+
+  OpenClock(){
+    this.isOpened = true;
+    this.openLock.play();
+
+    setTimeout(() => { 
+      this.keyObtainedSound.play();
+    },
+    3000);
+
+  }
 
 }

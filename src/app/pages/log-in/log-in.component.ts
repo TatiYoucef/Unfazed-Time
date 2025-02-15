4c245a62744297fafdda623b7ac4aa786260b99c
Tati Youcef <youcef.tati04@gmail.com>
2025-02-15 10:42:23 +0100

    temple is Templing

--- src/app/pages/log-in/log-in.component.ts
+++ src/app/pages/log-in/log-in.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoadingScreenComponent } from "../../components/loading-screen/loading-screen.component";
 
 @Component({
@@ -8,6 +8,43 @@ import { LoadingScreenComponent } from "../../components/loading-screen/loading-
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.css'
 })
-export class LogInComponent {
+export class LogInComponent implements OnInit{
+
+  isLoggedIn = false;
+
+  d = new Date() ;
+  h = this.d.getHours();
+  m = this.d.getMinutes();
+  s = this.d.getSeconds();
+
+  ngOnInit(): void {
+    this.clock();
+  }
+
+  clock(){
+
+    setTimeout(() => { 
+      this.clock();
+    },
+    1000);
+
+    this.s--;
+
+    if(this.s < 0){
+
+      this.m--;
+      this.s = 59;
+
+      if(this.m < 0){
+        this.h--;
+        this.m = 59;
+
+        if(this.h < 0){
+          this.h = 23;
+        }
+      }
+    }
+
+  }
 
 }

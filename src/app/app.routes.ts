1d74b0ffe268e5dc3a9309c75d9242cbd26e2177
Tati Youcef <youcef.tati04@gmail.com>
2025-07-21 17:33:10 +0100

    favicon test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -49,32 +49,32 @@ export const routes: Routes = [
     }
   },
 
-  // {
-  //   path :'home/dailyQuiz' , 
-  //   pathMatch: 'full', 
-  //   loadComponent: async () => {
-  //     const m = await import("./pages/EternalEnigma/daily-quiz/daily-quiz.component");
-  //     return m.DailyQuizComponent;
-  //   }
-  // },
+  {
+    path :'home/dailyQuiz' , 
+    pathMatch: 'full', 
+    loadComponent: async () => {
+      const m = await import("./pages/EternalEnigma/daily-quiz/daily-quiz.component");
+      return m.DailyQuizComponent;
+    }
+  },
 
-  // {
-  //   path :'home/dailyQuiz/hints' , 
-  //   pathMatch: 'full', 
-  //   loadComponent: async () => {
-  //     const m = await import("./pages/EternalEnigma/hints/hints.component");
-  //     return m.HintsComponent;
-  //   }
-  // },
+  {
+    path :'home/dailyQuiz/hints' , 
+    pathMatch: 'full', 
+    loadComponent: async () => {
+      const m = await import("./pages/EternalEnigma/hints/hints.component");
+      return m.HintsComponent;
+    }
+  },
 
-  // {
-  //   path :'home/dailyQuiz/progress' , 
-  //   pathMatch: 'full', 
-  //   loadComponent: async () => {
-  //     const m = await import("./pages/EternalEnigma/progress/progress.component");
-  //     return m.ProgressComponent;
-  //   }
-  // },
+  {
+    path :'home/dailyQuiz/progress' , 
+    pathMatch: 'full', 
+    loadComponent: async () => {
+      const m = await import("./pages/EternalEnigma/progress/progress.component");
+      return m.ProgressComponent;
+    }
+  },
 
   //-----------------------------ENIGMAS-------------------------
 
